c32af675cdb60c9d3871e2fb67716133a1982a07
daniel11jara <daniel11jara@gmail.com>
2025-01-27 22:43:48 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,4 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const amigo = document.getElementById('amigo');
+const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
